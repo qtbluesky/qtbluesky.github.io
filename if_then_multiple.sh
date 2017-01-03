35705b006a0a193b5ac4b9a79ec06b094ac3c8f7
qtbluesky <qtijay@126.com>
2017-01-03 16:47:48 +0800

    delete All

--- if_then_multiple.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#testing multiple commands in the then section
-testuser=qtbluesky
-if grep $testuser /etc/passwd
-then
-	echo The bash files for user $testuser are:
-	ls -a /home/$testyser/.b*
-fi
--- strcmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#testing string equality
-testuser=qtbluesky
-if [ $USER = $testuser ]
-	then
-	echo "welcome, $testuser"
-fi
